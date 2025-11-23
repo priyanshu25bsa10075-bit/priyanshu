af6ba947235b846100a9b8513a3665eae1cac660
priyanshu25bsa10075-bit <priyanshu.25bsa10075@vitbhopal.ac.in>
2025-11-24 02:19:50 +0530

    Update library_management.py

--- library_management.py
+++ library_management.py
@@ -1,75 +1,112 @@
+import gradio as gr
+
 class Library:
-    def __init__(self, list, name):
-        self.booksList = list
+    def __init__(self, books_list, name):
+        self.booksList = books_list  # List of tuples: (title, url)
         self.name = name
         self.lendDict = {}
 
     def displayBooks(self):
-        print(f"We have following books in our library: {self.name}")
-        for book in self.booksList:
-            print(book)
+        # Return markdown list with clickable links
+        md = "\n".join(
+            [f"- [{title}]({url})" for (title, url) in self.booksList]
+        )
+        return md
 
-    def lendBook(self, user, book):
-        if book not in self.lendDict.keys():
-            self.lendDict.update({book:user})
-            print("Lender-Book database has been updated. You can take the book now")
+    def lendBook(self, user, book_title):
+        # Check if book is in library
+        titles = [t for t, _ in self.booksList]
+        if book_title not in titles:
+            return "Sorry, this book is not in our library."
+
+        # Check if book is already lent out
+        if book_title not in self.lendDict:
+            self.lendDict[book_title] = user
+            return f"Lender-Book database updated. {user} can take '{book_title}' now."
         else:
-            print(f"Book is already being used by {self.lendDict[book]}")
+            return f"Book is already being used by {self.lendDict[book_title]}."
 
-    def addBook(self, book):
-        self.booksList.append(book)
-        print("Book has been added to the book list")
+    def addBook(self, title, url):
+        # Check if book already exists
+        titles = [t for t, _ in self.booksList]
+        if title in titles:
+            return "This book already exists in the library."
+        self.booksList.append((title, url))
+        return f"Book '{title}' has been added to the library."
 
-    def returnBook(self, book):
-        self.lendDict.pop(book)
-
-if __name__ == '__main__':
-    Stuti = Library(['-> Discrete Mathematics', '-> Data Structure', '-> Human Values', '-> Maths IV', '-> Computer System Architecture'], "Stuti's_Library")
-
-    while(True):
-        print("*****************************")
-        print("LIBRARY MANAGEMENT SYSTEM")
-        print("*****************************")
-        print("Enter your choice to continue")
-        print("1. Display Books")
-        print("2. Lend a Book")
-        print("3. Add a Book")
-        print("4. Return a Book")
-        user_choice = input()
-        if user_choice not in ['1','2','3','4']:
-            print("Please enter a valid option")
-            continue
-
+    def returnBook(self, book_title):
+        if book_title in self.lendDict:
+            self.lendDict.pop(book_title)
+            return f"'{book_title}' has been returned. Thank you!"
         else:
-            user_choice = int(user_choice)
+            return "This book was not lent out."
 
+# Initialize a large book database with real URLs
+initial_books = [
+    ("Discrete Mathematics", "https://en.wikipedia.org/wiki/Discrete_mathematics"),
+    ("Data Structure", "https://www.geeksforgeeks.org/data-structures/"),
+    ("Human Values", "https://www.scribd.com/doc/26365549/Human-Values-Character-Education"),
+    ("Maths IV", "https://nptel.ac.in/courses/111104032/"),
+    ("Computer System Architecture", "https://en.wikipedia.org/wiki/Computer_architecture"),
+    # Add more books with URLs as needed
+    ("Introduction to Algorithms", "https://mitpress.mit.edu/books/introduction-algorithms-third-edition"),
+    ("Python Programming", "https://docs.python.org/3/tutorial/"),
+    ("Artificial Intelligence", "https://www.aaai.org/"),
+    ("Machine Learning", "https://www.coursera.org/learn/machine-learning"),
+    ("Operating Systems", "https://pages.cs.wisc.edu/~remzi/OSTEP/"),
+    ("Database Management Systems", "https://dbms-book.com/"),
+]
 
-        if user_choice == 1:
-            Stuti.displayBooks()
+lib = Library(initial_books, "Stuti's Huge Library")
 
-        elif user_choice == 2:
-            book = input("Enter the name of the book you want to lend:")
-            user = input("Enter your name")
-            Stuti.lendBook(user, book)
+def library_app(action, user, book_title, book_url):
+    if action == "Display Books":
+        return lib.displayBooks()
+    elif action == "Lend a Book":
+        if not user or not book_title:
+            return "Please enter both user name and book title for lending."
+        return lib.lendBook(user, book_title)
+    elif action == "Add a Book":
+        if not book_title or not book_url:
+            return "Please enter both book title and URL to add a book."
+        return lib.addBook(book_title, book_url)
+    elif action == "Return a Book":
+        if not book_title:
+            return "Please enter the book title to return."
+        return lib.returnBook(book_title)
+    else:
+        return "Please select a valid action."
 
-        elif user_choice == 3:
-            book = input("Enter the name of the book you want to add:")
-            Stuti.addBook(book)
+with gr.Blocks() as demo:
+    gr.Markdown("# Library Management System with Large Book Database and Real Links")
 
-        elif user_choice == 4:
-            book = input("Enter the name of the book you want to return:")
-            Stuti.returnBook(book)
+    action = gr.Radio(
+        ["Display Books", "Lend a Book", "Add a Book", "Return a Book"],
+        label="Select Action",
+    )
 
-        else:
-            print("Not a valid option")
+    user = gr.Textbox(label="User Name (for lending books)", placeholder="Enter your name")
+    book_title = gr.Textbox(label="Book Title", placeholder="Enter book title")
+    book_url = gr.Textbox(label="Book URL (only for adding books)", placeholder="Enter book URL")
 
+    output = gr.Markdown()
 
-        print("Press q to quit and c to continue")
-        user_choice2 = ""
-        while(user_choice2!="c" and user_choice2!="q"):
-            user_choice2 = input()
-            if user_choice2 == "q":
-                exit()
+    def update_ui(action_choice):
+        # Show/hide user and URL inputs based on action
+        user.visible = action_choice == "Lend a Book"
+        book_url.visible = action_choice == "Add a Book"
 
-            elif user_choice2 == "c":
-                continue
+    action.change(update_ui, inputs=action)
+
+    submit_btn = gr.Button("Submit")
+
+    def on_submit(action_choice, user_name, title, url):
+        return library_app(action_choice, user_name, title, url)
+
+    submit_btn.click(
+        on_submit,
+        inputs=[action, user, book_title, book_url],
+        outputs=output,
+    )
+
+demo.launch(share=True)
